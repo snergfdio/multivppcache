f2a9d15fb92d00541e0e5e5039c91480d7ce8b6c
juraj.linkes <juraj.linkes@pantheon.tech>
2019-07-01 10:27:12 +0200

    Remove unnecessary layers from csit ubuntu images
    Signed-off-by: juraj.linkes <juraj.linkes@pantheon.tech>

--- csit-sut/get-linux-vm-image.sh
+++ csit-sut/get-linux-vm-image.sh
@@ -8,8 +8,10 @@ else
     KERNEL_IMG_PACKAGE='linux-image-kvm'
 fi
 
+apt-get update
 cd ${TARGET_DIR}
 KERNEL_IMG_PACKAGE=$(apt-cache depends -i ${KERNEL_IMG_PACKAGE} | grep Depends: | cut -d ' ' -f4)
 apt-get -y download ${KERNEL_IMG_PACKAGE}
 dpkg --extract ${KERNEL_IMG_PACKAGE}_*.deb ${TARGET_DIR}
 cd -
+rm -rf /var/lib/apt/lists/*
